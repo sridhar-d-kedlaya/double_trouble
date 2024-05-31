561e177e1f6a653f1d9bb0c15469430ab2d8da2b
Sridhar D Kedlaya <sridhardked@gmail.com>
2024-05-31 17:19:23 +0530

    Add shell.nix

--- shell.nix
+++ shell.nix
@@ -3,8 +3,14 @@
 pkgs.mkShell
 {
   nativeBuildInputs = [
+    pkgs.trashy
+    pkgs.python312
+    pkgs.python312Packages.pip
+    pkgs.python312Packages.virtualenv
   ];
   
   shellHook = ''
+    source .venv/bin/activate
+    alias rm="trash -c always put"
   '';
 }
